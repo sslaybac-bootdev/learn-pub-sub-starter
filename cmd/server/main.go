f4a0ac83d8f6a063b24fbdbdfd248328f9a63326
Mike Slayback <Scott.M.Slayback@gmail.com>
2026-08-20 09:59:54 -0700

    2.3 Simple framework for rabbit connection.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	ampqClient, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ampqClient.Close()
+	fmt.Println("RabbitMQ connection successful")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting Down...")
+
 }
